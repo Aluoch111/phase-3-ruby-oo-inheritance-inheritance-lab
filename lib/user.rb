cd1de6c9e6b390dd3d73c4293207be45acd52d20
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-10-27 18:14:17 +0300

    complete

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+    
+   attr_accessor :first_name, :last_name
+
+    def name(first_name,last_name)
+        @firstname = first_name
+        @last_name = last_name
+    end
+    
+end
